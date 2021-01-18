7729bfed89724eb850968c5dbb7b62512c4ffe7c
David Berg <berg.david1@mayo.edu>
2021-01-18 16:05:45 -0600

    Limit negative logic. Flatten logic nesting

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+def is_expired_item(item):
+    return
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,33 +11,34 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.sell_in < 11:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+            elif item.name == "Aged Brie":
+                item.quality = min(item.quality + 2, 50)
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+
+            if item.sell_in < 0:
+                if item.name in {"Sulfuras, Hand of Ragnaros", "Aged Brie"}:
+                    pass
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = item.quality - item.quality
+                else:
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
 
 
 class Item:
